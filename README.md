8e1cb1dc9403deb71108f8ceb581a3b8e3d056dc
litong <ghostong@126.com>
2020-03-18 15:14:54 +0800

    增加readme

---
+++ README.md
@@ -0,0 +1,12 @@
+媒体处理相关
+==============
+媒体处理
+
+### blurOut 不足比例的视频/图片虚化
+````php
+//处理为 1:1 的虚化图片
+(new Lit\Media\BlurOut("in.jpg"))->exec(1,1,"./out.jpg");
+
+//处理为为 4:5 的虚化视频
+(new Lit\Media\BlurOut("in.mp4"))->exec(4,5,"./out.mp4");
+````
--- demo.php
+++ demo.php
@@ -3,7 +3,7 @@
 include("./vendor/autoload.php");
 
 //处理为 1:1 的虚化图片
-(new Lit\Media\BlurOut("a.jpg"))->exec(1,1,"./out.jpg");
+(new Lit\Media\BlurOut("in.jpg"))->exec(1,1,"./out.jpg");
 
 //处理为为 4:5 的虚化视频
-(new Lit\Media\BlurOut("a.mp4"))->exec(4,5,"./out.mp4");
+(new Lit\Media\BlurOut("in.mp4"))->exec(4,5,"./out.mp4");
